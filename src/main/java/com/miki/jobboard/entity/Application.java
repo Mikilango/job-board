12541a717d7fb842248b6b958bf96b404bc0d297
Mirlanbek <117597589+Mikilango@users.noreply.github.com>
2026-04-05 17:21:22 +0600

    feat: add Application entity and ApplicationStatus enum

---
+++ src/main/java/com/miki/jobboard/entity/Application.java
@@ -0,0 +1,41 @@
+package com.miki.jobboard.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "applications")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus status;
+
+    @Column(nullable = false)
+    private String coverLetter;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id", nullable = false)
+    private Job job;
+
+}
