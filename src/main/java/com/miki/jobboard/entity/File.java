0b82af10da0c007c4e63142d1eccd9339ac80f9a
Mirlanbek <117597589+Mikilango@users.noreply.github.com>
2026-04-05 23:35:09 +0600

    feat: add File entity

---
+++ src/main/java/com/miki/jobboard/entity/File.java
@@ -0,0 +1,31 @@
+package com.miki.jobboard.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "file")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    private String fileType;
+
+    @OneToOne
+    @JoinColumn(name = "application_id", nullable = false)
+    private Application application;
+}
