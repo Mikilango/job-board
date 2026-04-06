bd19feee5dc606a9f3be0936ce366195aeb778ba
Mirlanbek <117597589+Mikilango@users.noreply.github.com>
2026-04-06 17:53:16 +0600

    feat: add JwtUtil

---
+++ src/main/java/com/miki/jobboard/security/JwtUtil.java
@@ -0,0 +1,54 @@
+package com.miki.jobboard.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)           // кладём email в токен
+                .setIssuedAt(new Date())     // когда создан
+                .setExpiration(new Date(System.currentTimeMillis() + expiration)) // когда истекает
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256) // подписываем
+                .compact();                  // собираем в строку
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();  // достаём email
+    }
+
+    public boolean isTokenExpired(String token) {
+        Date expiredDate = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+        return expiredDate.before(new Date());
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
+
